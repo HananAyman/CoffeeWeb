41da8b138f5328d1cd99f68e54a23b677ce40f8f
Hanan_000 <Hanan_000@SWD-08>
2017-02-06 14:33:40 +0200

    new commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$title = "Home";
+$content = '<img src="images/coffee1.png" class="imgLeft">
+<h1> What is coffee ?</h1>
+<p>
+ Coffee is a brewed drink prepared from roasted coffee beans, which are the seeds of berries from the Coffea plant. The genus Coffea is native to tropical Africa, and Madagascar, the Comoros, Mauritius and Réunion in the Indian Ocean.The plant was exported from Africa to countries around the world and coffee plants are now cultivated in over 70 countries, primarily in the equatorial regions of the Americas, Southeast Asia, India, and Africa. The two most commonly grown are the highly regarded arabica, and the less sophisticated but stronger and more hardy robusta. Once ripe, coffee berries are picked, processed, and dried. Dried coffee seeds (referred to as beans) are roasted to varying degrees, depending on the desired flavor. Roasted beans are ground and brewed with near boiling water to produce coffee as a beverage.
+</p>
+
+<img src="images/coffee2.png" class="imgRight">
+<h1> what is benefits of it? </h1>
+<p>
+Cut the Pain
+Two cups of coffee can cut post-workout muscle pain by up to 48%. From the Journal of Pain, March 2007 (link)
+Increase your fiber intake
+A cup of brewed coffee represents a contribution of up to 1.8 grams of fiber of the recommended intake of 20-38 grams. From the Journal of Agricultural and Food Chemistry (link).
+Protection against cirrhosis of the liver
+Of course, you could just cut down on the alcohol intake. From the Archives of Internal Medicine (link). Another more recent study also showed coffee’s liver protecting benefits. link. Yet another study showed that both coffee and decaffeinated coffee lowered the liver enzyme levels of coffee drinkers. This study was published in the Hepatology Journal.
+Lowered risk of Type 2 Diabetes
+Those who consumed 6 or more cups per day had a 22% lower risk of diabetes. From the Archives of Internal Medicine (link). A recent review of research conducted by Harvard’s Dr. Frank Hu showed that the risk of type II diabetes decreases by 9% for each daily cup of coffee consumed. Decaf coffee decreased risk by 6% per cup.
+Lowered risk of Alzheimer’s disease
+There is considerable evidence that caffeine may protect against Alzheimer’s disease. From the European Journal of Neurology (link).
+Reduces suicide risk and Depression.
+</p>
+
+<h2> Another beneifts of It </h2>
+<img src="images/coffee3.png" class="imgLeft">
+<p>
+A 10-year study of 86,000 female nurses shows a reduced risk of suicide in the coffee drinkers. From the Archives of Internal Medicine (link). Another study conducted by the Harvard School of Public Health found that women who drink 4 or more cups of coffee were 20% less likely to suffer from depression. Study link.
+Protection against Parkinson’s
+People with Parkinson’s disease are less likely to be smokers and coffee drinkers than their healthy siblings. Just make sure you don’t get lung cancer on the way. From the Archives of Neurology (link). Even newer research out of Sweden revealed that drinking coffee reduces the risk of Parkinson’s even when genetic factors come into play. link.
+Coffee drinkers have less risk of heart disease. Korean researchers found that study participants who consumed 3 to 5 cups of coffee a day were less likely to show the beginning signs of heart disease. The study. Other dietary factors should also be noted as Koreans typically have a different diet than do Westerners.
+Coffee drinkers have stronger DNA. A study published in the European Journal of Nutrition showed that coffee drinkers have DNA with stronger integrity since the white blood cells of coffee drinkers had far less instance of spontaneous DNA strand breakage. Study abstract.
+Lower Risk of Multiple Sclerosis. Recent research showed that at least 4 cups of coffee a day may help protect against the development and reoccurrence of MS. It is believed that the coffee prevents the neural inflammation that possibly leads to the disease developing. The study was published in the Journal of Neurology, Neurosurgery & Psychiatry. 
+Coffee reduces colorectal cancer risk. Even moderate consumption of coffee can reduce the odds of developing colorectal cancer by 26%. This protective benefit increases with more consumption. 
+</p>';
+include 'Template.php';
+?>
